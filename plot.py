db56de668f704cce0a514162f9ccf335f3c69abd
Hadiur Thasin <hadiurthasin@MacBook-Pro-247.local>
2025-03-23 01:55:36 -0400

    imported lib got plot.py to work

---
+++ plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_values = [1,2,3]
+
+x_valuess = [3,2,1]
+
+y_values = [2,3,4]
+
+john = plt.plot(x_values,y_values)
+
+sam =  plt.plot(x_valuess,y_values)
+
+plt.show()
+
+
